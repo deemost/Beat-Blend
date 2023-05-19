5e8b9f5a0841103c63bf0d123fdbddd2fd371639
Nadeem Baba <nadeem@nadeems-mbp.myfiosgateway.com>
2023-05-19 00:59:21 -0400

    Fixed queue / don't need to press next queue button anymore.

--- client/src/CustomSpotifyPlayer.js
+++ client/src/CustomSpotifyPlayer.js
@@ -1,58 +1,49 @@
-import { useState, useEffect } from "react"
-import SpotifyPlayer from "react-spotify-web-playback"
+import { useState, useEffect } from "react";
+import SpotifyPlayer from "react-spotify-web-playback";
 
 export default function Player({ accessToken, trackUri, playNextInTheQueue }) {
-  const [play, setPlay] = useState(false)
-  const [accum, setAccum] = useState(0)
-  const [prevTrack, setPrevTrack] = useState("")
+  const [play, setPlay] = useState(false);
+  const [accum, setAccum] = useState(-3);
+  const [prevTrack, setPrevTrack] = useState("");
 
-  useEffect(() => setPlay(true), [trackUri])
-  if (!accessToken) return null
-
-
+  useEffect(() => setPlay(true), [trackUri]);
+  if (!accessToken) return null;
 
   return (
     <SpotifyPlayer
       token={accessToken}
-
-      callback={state => {
+      callback={(state) => {
         if (!state.isPlaying) setPlay(false);
-        console.log('-------- player state: ' + JSON.stringify(state));
+        console.log("-------- player state: " + JSON.stringify(state));
 
-        if(prevTrack != state.name){
+        if (prevTrack != state.name) {
           setPrevTrack(state.name);
         }
 
-
-        else if(state.position == 0 && accum == 0){
+        if (state.position == 0 && accum >= 1) {
+          playNextInTheQueue();
+          setAccum(-1);
+        } else if (state.position == 0) {
           setAccum(accum + 1);
         }
-        else if(state.position == 0 && accum != 0){
-          playNextInTheQueue();
-          setAccum(0);
-        }
-  
 
-        console.log("WWWWWWWcur" + state.position );
+        console.log("WWWWWWWcur" + state.position);
         console.log("Thing " + accum);
-
       }}
-
-      
       play={play}
+      autoPlay={true}
       uris={trackUri ? [trackUri] : []}
-
       // queue={queue}
 
       styles={{
-        activeColor: '#fff',
-        bgColor: '#333',
-        color: '#fff',
-        loaderColor: '#fff',
-        sliderColor: '#1cb954',
-        trackArtistColor: '#ccc',
-        trackNameColor: '#fff',
+        activeColor: "#fff",
+        bgColor: "#333",
+        color: "#fff",
+        loaderColor: "#fff",
+        sliderColor: "#1cb954",
+        trackArtistColor: "#ccc",
+        trackNameColor: "#fff",
       }}
     />
-  )
+  );
 }
