0d16f0f7f319aced6dbfbe861c4df4c6afd82ba4
deemost <nadeem.baba114@gmail.com>
2024-01-27 13:55:30 -0500

    Fixed home page slighlty

--- client/src/Home.js
+++ client/src/Home.js
@@ -7,38 +7,62 @@ export default function Home() {
     let history = useNavigate();
 
     const [room, setRoom] = useState("")
+    const [username, setUsername] = useState("")
 
-    const handleSubmit = () => {
+    const handleSubmitRoom = () => {
         // e.preventDefault();
         // e.stopPropagation();
         history("/room/" + room);
     };
 
-    const handleChange = e => {
+    const handleChangeRoom = e => {
         setRoom(e.target.value)
+    };
+
+    const handleSubmitUsername = () => {
+        setUsername(username)
+    };
+
+    const handleChangeUsername = e => {
+        setUsername(e.target.value)
     };
 
 
     return (
-        <div>
+        <div className="d-flex align-items-center justify-content-center">
             <Container>
-                <Row>
-                    <Col><h1>Beat Blend</h1></Col>
-                </Row>
-                <Row>
+                <Row className="text-center">
                     <Col>
-                        <Link to={`host`} className="btn btn-outline-success btn-lg">Enter as a Host</Link>
+                        <h1>Beat Blend</h1>
                     </Col>
-                    <Col>
+                </Row>
+                <Row className="justify-content-center mt-lg-5">
+                    <Col className="justify-content-center" xs={12} md={6} lg={4}>
                         <Form>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Control type="text" placeholder="Room #" onChange={handleChange}/>
-                                <Button onClick={handleSubmit}>Go</Button>
+                            <Form.Group as={Row}>
+                                <Col xs={8} md={8} lg={8}>
+                                    <Form.Control type="text" placeholder="Username" onChange={handleChangeUsername} />
+                                </Col>
+                                <Col>
+                                    <Button onClick={handleSubmitUsername}>Go</Button>
+                                </Col>
+                            </Form.Group>
+                        </Form>
+                        <Link to={`host`} className="btn btn-outline-success btn-lg">Enter as a Host</Link>
+                        <Form>
+                            <Form.Group as={Row}>
+                                <Col xs={8} md={8} lg={8}>
+                                    <Form.Control type="text" placeholder="Room #" onChange={handleChangeRoom} />
+                                </Col>
+                                <Col>
+                                    <Button onClick={handleSubmitRoom}>Go</Button>
+                                </Col>
                             </Form.Group>
                         </Form>
                     </Col>
                 </Row>
             </Container>
         </div>
+
     )
 }
